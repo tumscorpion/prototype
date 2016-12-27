c5085b410be809b9a6698314a96ad762747a0874
tumscorpion <prateep2007@gmail.com>
2016-12-27 11:28:33 +0700

    init

---
+++ assets/pages/scripts/table-datatables-responsive.js
@@ -0,0 +1,87 @@
+var TableDatatablesResponsive = function () {
+
+    var initTable1 = function () {
+        var table = $('#sample_1');
+
+        var oTable = table.dataTable({
+            // Internationalisation. For more info refer to http://datatables.net/manual/i18n
+            "language": {
+                "aria": {
+                    "sortAscending": ": activate to sort column ascending",
+                    "sortDescending": ": activate to sort column descending"
+                },
+                "emptyTable": "No data available in table",
+                "info": "Showing _START_ to _END_ of _TOTAL_ entries",
+                "infoEmpty": "No entries found",
+                "infoFiltered": "(filtered1 from _MAX_ total entries)",
+                "lengthMenu": "_MENU_ entries",
+                "zeroRecords": "No matching records found"
+            },
+
+            // Or you can use remote translation file
+            //"language": {
+            //   url: '//cdn.datatables.net/plug-ins/3cfcc339e89/i18n/Portuguese.json'
+            //},
+
+            // setup buttons extentension: http://datatables.net/extensions/buttons/
+            buttons: [
+               
+            ],
+
+            // setup responsive extension: http://datatables.net/extensions/responsive/
+            responsive: {
+                details: {
+                   
+                }
+            },
+
+            "order": [
+                [1, 'asc']
+            ],
+            
+            "lengthMenu": [
+                [5, 10, 15, 20, -1],
+                [5, 10, 15, 20, "All"] // change per page values here
+            ],
+            // set the initial value
+            "pageLength": 5,
+
+            "searching": false,
+
+            "columnDefs": [ {
+                "targets": [0,4,5],
+                "orderable": false
+            },
+        
+            {"className": "dt-center", "targets": [0]},
+            {"className": "dt-head-center", "targets": [1,2,3,4]},
+            ],
+
+            // "dom": "<'row' <'col-md-12'B>><'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r><'table-scrollable't><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>", // horizobtal scrollable datatable
+
+            // Uncomment below line("dom" parameter) to fix the dropdown overflow issue in the datatable cells. The default datatable layout
+            // setup uses scrollable div(table-scrollable) with overflow:auto to enable vertical scroll(see: assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js). 
+            // So when dropdowns used the scrollable div should be removed. 
+            //"dom": "<'row' <'col-md-12'T>><'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r>t<'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
+        });
+    }
+
+    return {
+
+        //main function to initiate the module
+        init: function () {
+
+            if (!jQuery().dataTable) {
+                return;
+            }
+
+            initTable1();
+        }
+
+    };
+
+}();
+
+jQuery(document).ready(function() {
+    TableDatatablesResponsive.init();
+});
